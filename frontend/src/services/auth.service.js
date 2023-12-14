594d02892378d34116c70f9f2cd8f5482c8d950a
Cristofer <cristofer.mellado2101@alumnos.ubiobio.cl>
2023-12-14 12:56:58 -0300

    Solucion error de CORS

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -29,14 +29,4 @@ export const logout = () => {
   cookies.remove('jwt-auth');
 };
 
-export const test = async () => {
-  try {
-    const response = await axios.get('/users');
-    const { status, data } = response;
-    if (status === 200) {
-      console.log(data.data);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+
